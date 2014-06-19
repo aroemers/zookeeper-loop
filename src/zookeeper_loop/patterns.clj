35a53d0c9ccc938b211528defdc119f844c4d317
Arnout Roemers <github@functionalbytes.nl>
2014-06-19 17:32:08 +0200

    Simplify patterns/swap! of zookeeper-loop

--- src/zookeeper_loop/patterns.clj
+++ src/zookeeper_loop/patterns.clj
@@ -16,7 +16,10 @@
 
 
 (defn ensure-path
-  "Ensure the given path exists."
+  "Ensure the given path exists. When the connection to Zookeeper
+  cannot be (re)established within timeout-msec while processing the
+  path, an exception is thrown. Returns the path when created, or nil
+  when it already exists"
   [client-loop path timeout-msec persistent?]
   (loop [client (deref client-loop timeout-msec ::disconnected)]
     (if-not (= ::disconnected client)
@@ -34,24 +37,23 @@
   "Apply the given fn atomically to the data of the given path,
   together with the args. When the connection to Zookeeper cannot be
   (re)established within timeout-msec while processing the swap, an
-  exception is thrown. Returns the new value."
+  exception is thrown. Requires *deserializer* and *serializer* to be
+  bound to functions. Returns the new value."
   [client-loop path timeout-msec fn & args]
   (assert (fn? *deserializer*) "swap! needs *deserializer* to be bound to a function.")
   (assert (fn? *serializer*) "swap! needs *serializer* to be bound to a function.")
   (loop [client (deref client-loop timeout-msec ::disconnected)]
     (if-not (= ::disconnected client)
-      (let [data (try (zk/data client path)
-                      (catch KeeperException$SessionExpiredException see ::retry)
-                      (catch KeeperException$ConnectionLossException cle ::retry))]
-        (if (= ::retry data)
+      (let [new-data (try (let [data (zk/data client path)
+                                deserialized (when-let [bytes (:data data)] (*deserializer* bytes))
+                                new-data (apply fn deserialized args)
+                                version (-> data :stat :version)]
+                            (zk/set-data client path (*serializer* new-data) version)
+                            new-data)
+                          (catch KeeperException$BadVersionException bve ::retry)
+                          (catch KeeperException$SessionExpiredException see ::retry)
+                          (catch KeeperException$ConnectionLossException cle ::retry))]
+        (if (= ::retry new-data)
           (recur (deref client-loop timeout-msec ::disconnected))
-          (let [new-data (apply fn (when-let [bytes (:data data)] (*deserializer* bytes)) args)
-                version (-> data :stat :version)
-                result (try (zk/set-data client path (*serializer* new-data) version)
-                            (catch KeeperException$BadVersionException bve ::retry)
-                            (catch KeeperException$SessionExpiredException see ::retry)
-                            (catch KeeperException$ConnectionLossException cle ::retry))]
-            (if (= ::retry result)
-              (recur (deref client-loop timeout-msec ::disconnected))
-              new-data))))
+          new-data))
       (ex-info "Could not connect to Zookeeper for swap." {:client @(:client-atom client)}))))
