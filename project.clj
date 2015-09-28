84e0a380c9515df790cbefe111bc2f3d2fde0664
Arnout Roemers <github@functionalbytes.nl>
2015-09-28 10:38:41 +0200

    Update version and dependencies
    Removed the need to test with a curator profile

--- project.clj
+++ project.clj
@@ -2,17 +2,14 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-(defproject functionalbytes/zookeeper-loop "0.1.0-SNAPSHOT"
+(defproject functionalbytes/zookeeper-loop "0.1.0"
   :description "An automatically reconnecting Zookeeper client."
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [zookeeper-clj "0.9.1"]
-                 [com.taoensso/timbre "3.2.1"]]
-  :profiles {;; Silly profile needed, otherwise mvn won't include zookeeper in its compile
-             ;; phase dependencies. To test, call `lein with-profile +curator test`.
-             :curator {:dependencies [[org.apache.zookeeper/zookeeper "3.4.5" :exclusions
-                                       [com.sun.jmx/jmxri com.sun.jdmk/jmxtools javax.jms/jms junit]]
-                                      [org.apache.curator/curator-test "2.4.2"]]}}
-  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.15"
+                 [com.taoensso/timbre "3.2.1"]
+                 [org.apache.curator/curator-test "2.9.0" :scope "test"]
+                 [org.apache.zookeeper/zookeeper "3.4.6"]]
+  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.7.1"
                  {:extensions "true"
                   :executions ([:execution
                                 [:id "clojure-compile"]
@@ -20,5 +17,12 @@
                                 [:configuration
                                  [:temporaryOutputDirectory "true"]
                                  [:sourceDirectories [:sourceDirectory "src"]]]
-                                [:goals [:goal "compile"]]])}]]
+                                [:goals [:goal "compile"]]]
+                               [:execution
+                                [:id "clojure-test"]
+                                [:phase "test"]
+                                [:configuration
+                                 [:temporaryOutputDirectory "true"]
+                                 [:sourceDirectories [:sourceDirectory "test"]]]
+                                [:goals [:goal "test"]]])}]]
   :global-vars {*warn-on-reflection* true})
